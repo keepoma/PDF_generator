156f93373f14d9397ab31863fc786a9dc7b9c1c8
NikKar <nikita.karuzin@gmail.com>
2023-10-01 17:45:44 -0600

    Add lines

--- PFD_generator.py
+++ PFD_generator.py
@@ -13,7 +13,10 @@ for index, row in df.iterrows():
     pdf.set_font(family='Times', style='B', size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row['Topic'], border=0, ln=1)
-    pdf.line(10, 21, 200, 21)
+
+    # Create lines
+    for y in range(21, 290, 8):
+        pdf.line(10, y, 200, y)
 
     # Set the footer
     pdf.ln(265)
@@ -23,6 +26,10 @@ for index, row in df.iterrows():
 
     for i in range(row['Pages'] - 1):
         pdf.add_page()
+
+        # Create lines
+        for y in range(21, 290, 8):
+            pdf.line(10, y, 200, y)
 
         # Set the footer
         pdf.ln(277)
